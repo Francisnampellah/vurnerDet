20e09e0a2dcfe997f26ed6aa6c3a64cd3f413a56
francisnampellah <bnampellah1@gmail.com>
2025-05-13 13:06:22 +0300

    Update start.sh to use sudo for creating and setting permissions on acme.json: This change ensures proper permissions are applied when the file is created, enhancing security and functionality.

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 # Create acme.json if it doesn't exist
 if [ ! -f "./acme.json" ]; then
   echo "Creating acme.json..."
-  touch ./acme.json
-  chmod 600 ./acme.json
+  sudo touch ./acme.json
+  sudo chmod 600 ./acme.json
 fi
 
 # Start docker-compose
-docker-compose up -d --build
+sudo docker-compose up -d --build
